ace4bf8416540edf01afdbd21e92a4dda1ff78eb
dheerapat <dheerapat.t@kkumail.com>
2022-05-06 00:26:50 +0700

    first commit

---
+++ progress-bar.py
@@ -0,0 +1,16 @@
+import math
+
+
+def progress_bar(progress, total):
+    percent = 100 * (progress / float(total))
+    bar = '#' * int(percent)
+    print(f'\r{bar} {percent}', end='\r')
+
+
+numbers = [x * 5 for x in range(2000, 3000)]
+result = []
+
+progress_bar(0, len(numbers))
+for i, x in enumerate(numbers):
+    result.append(math.factorial(x))
+    progress_bar(i + 1, len(numbers))
